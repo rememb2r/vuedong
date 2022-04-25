31b5ed94d2e5af7e7247ab8816ce8123f2410ba2
Jonghwan Zhu <jonghwanzhu@Jonghwanui-MacBookAir.local>
2022-04-25 23:37:23 +0900

    some

--- src/Card.vue
+++ src/Card.vue
@@ -1,7 +1,6 @@
 <template>
   <div @click="$emit('openModal', oneRoom.id)">
     <p>{{ oneRoom.id }}</p>
-
     <img class="oneroom-image" :src="oneRoom.image" />
     <h4 @click="$emit('openModal', oneRoom.id)">{{ oneRoom.title }}</h4>
     <h4>{{ oneRoom.price }}원</h4>
@@ -9,7 +8,6 @@
 </template>
 
 <script>
-
   export default {
     name: "Card",
     props: {
@@ -17,7 +15,6 @@
     indexClicked: Number,
     },
   };
-
 </script>
 
 
