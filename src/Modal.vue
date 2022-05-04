0660eabe4d415e7e0cb666609a45133634557174
Zhu Jonghwan <jonghwan@localhost.localdomain>
2022-05-04 17:47:40 +0900

    watcher  강 의 완료

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="black-bg" v-if="isModalOpened == true">
-  <div class="white-bg" @click="$emit('closeModal')">
-    <h2>{{ oneRoomDatas[indexClicked].title }}</h2>
-    <img class="oneroom-modal-image" :src="oneRoomDatas[indexClicked].image"/>
-  <h4>{{ oneRoomDatas[indexClicked].content }}</h4>
-  <p>{{ oneRoomDatas[indexClicked].price }}</p>
-  </div>
+    <div class="white-bg">
+      <h2>{{ oneRoomDatas[indexClicked].title }}</h2>
+      <img class="oneroom-modal-image" :src="oneRoomDatas[indexClicked].image"/>
+      <h4>{{ oneRoomDatas[indexClicked].content }}</h4>
+      <p>{{month}}개월 선택함 : {{ oneRoomDatas[indexClicked].price * (month > 1 && month < 99 ? month : 1) }}</p>
+      <input v-model.number="month"/>
+      <br><br>
+      <button @click="$emit('closeModal')">닫기</button>
+    </div>
   </div>
 </template>
 
@@ -13,10 +16,20 @@
 
   export default {
     name: "Modal",
+    data() {
+      return {
+        month : 1,
+      }
+    },
+    watch : {
+      month(){
+
+      },
+    },
     props: {
-    oneRoomDatas: Array,
-    isModalOpened: Boolean,
-    indexClicked: Number,
+      oneRoomDatas: Array,
+      isModalOpened: Boolean,
+      indexClicked: Number,
     },
 
   };
